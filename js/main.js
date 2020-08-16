1f2acc4f7bdcda9ac62607f42e474fc891285944
haziq <haziq.othman99@gmail.com>
2020-08-16 23:26:08 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const tabItems = document.querySelectorAll('.tab-item')
+const tabContentItems = document.querySelectorAll('.tab-content-item')
+
+//select tab content item
+function selectItem(e) {
+    //Add border to current tab
+    this.classList
+}
+
+
+//Listen for tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
