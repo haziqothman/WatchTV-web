e73eba0147a89aeeebfc2778c31cf823e30a7fb2
haziq <haziq.othman99@gmail.com>
2020-08-16 23:58:23 +0800

    tab-content-funtion

--- js/main.js
+++ js/main.js
@@ -3,10 +3,23 @@ const tabContentItems = document.querySelectorAll('.tab-content-item')
 
 //select tab content item
 function selectItem(e) {
+    removeBorder();
+    removeShow();
     //Add border to current tab
-    this.classList
+    this.classList.add('tab-border');
+    //Grab content form DOM
+    const tabContentItem = document.querySelector(`#${this.id}-content`)
+    //add show class
+    tabContentItem.classList.add('show');
 }
 
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'))
+}
+
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'))
+}
 
 //Listen for tab click
 tabItems.forEach(item => item.addEventListener('click', selectItem));
